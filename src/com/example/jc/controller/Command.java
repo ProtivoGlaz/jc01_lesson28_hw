52352a511ba36876741dada9bf22a8c13c62adf1
ProtivoGlaz <51762853+ProtivoGlaz@users.noreply.github.com>
2025-06-25 19:06:53 +0300

    Delete src directory

--- src/com/example/jc/controller/Command.java
+++
@@ -1,5 +0,0 @@
-package com.example.jc.controller;
-
-public interface Command {
-    String execute(String request) throws CourseControllerException;
-}
--- src/com/example/jc/controller/CommandName.java
+++
@@ -1,5 +0,0 @@
-package com.example.jc.controller;
-
-public enum CommandName {
-    CREATE_COURSE, ADD_PARTICIPANT, SHOW_ALL_COURSES, WRONG_REQUEST
-}
